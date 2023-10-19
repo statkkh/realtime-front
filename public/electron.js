a3b4a52c9d448045088ac90ac593d4ab5ae5f544
khkim <stakkh4@gmail.com>
2023-10-19 12:40:08 +0900

    application create - full screen

--- public/electron.js
+++ public/electron.js
@@ -3,7 +3,9 @@ const {app, BrowserWindow } = require('electron');
 const createWindow = () =>{
     const window = new BrowserWindow({
         width: 1200,
-        height : 800
+        height : 800,
+        autoHideMenuBar:true,
+        fullscreen : true
     });
     window.loadURL('http://localhost:3000');
 }
@@ -14,7 +16,7 @@ app.on('window-all-closed', ()=> {
     if(process.platform !== 'darwin') app.quit();
 });
 
-// 열린 윈도우개수가 0이면 새로운 윈도우 열기
+// description : 열린 윈도우개수가 0이면 새로운 윈도우 열기
 app.on('activate', () => {
     if(BrowserWindow.getAllWindows().length === 0) createWindow();
 })
